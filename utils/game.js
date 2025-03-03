266d7e1520a5863daafb21b5cf56ba15fdb63d11
Paola MAUCERI <paola.mauceri7@gmail.com>
2025-03-03 22:34:30 +0100

    fix(errors): improve handle of errors

--- utils/game.js
+++ utils/game.js
@@ -1,5 +1,5 @@
 const getTop100Games = (os) => fetch(`https://interview-marketing-eng-dev.s3.eu-west-1.amazonaws.com/${os}.top100.json`)
     .then(res => res.json())
-    .then(data => data.reduce((acc, group) => [...acc, ...group], []))
+    .then(data => data.reduce((acc, group) => [...acc, ...group], []).slice(0, 100))
 
 module.exports = { getTop100Games }
